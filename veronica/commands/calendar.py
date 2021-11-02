0a657252bc9c6b6159c479428295982078a68092
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-11-02 22:25:07 +0530

    feat: fetching events from all calendars

--- veronica/commands/calendar.py
+++ veronica/commands/calendar.py
@@ -5,7 +5,7 @@ from veronica.config import component
 from veronica.voice import vx_print
 from googleapiclient.discovery import build
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 @component
 def do_calendar(self,args):
@@ -21,15 +21,34 @@ def do_calendar(self,args):
         creds = self.vx_google_setup(self.SCOPES)
     service = build('calendar', 'v3', credentials=creds)
 
-    now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=5, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-    for i in events:
-        start= i['start']['dateTime'].split("+")[0]
-        start= datetime.strptime(start,"%Y-%d-%mT%H:%M:%S")
-        end= i['end']['dateTime'].split("+")[0]
-        end= datetime.strptime(end,"%Y-%d-%mT%H:%M:%S")
-        print(f"{start.strftime('%-m %b, %-I:%M %p'): <30} {i['summary']: <60}")
-    # print(events)
+    now = datetime.utcnow().isoformat() + 'Z'
+    maxLimit= (datetime.utcnow()+timedelta(hours=72)).isoformat() + 'Z'
+
+    calendars= service.calendarList().list().execute().get("items",[])
+    events=[]
+    for calendar in calendars:
+        if(calendar.get("selected",False)==True):
+            cal_events= service.events().list(calendarId=calendar["id"], timeMin=now, timeMax= maxLimit,
+                                            maxResults=2500).execute().get('items',[])
+            for cal_event in cal_events:
+                new_event={
+                    "link": cal_event["htmlLink"],
+                    "title": cal_event["summary"],
+                    "calendar": calendar.get("summaryOverride",None) or calendar.get("summary",None),
+                    "color": calendar["backgroundColor"]
+                }
+
+                # If date is present in start or end times, it means event is all day. So the same thing replicates
+                # for end date as well.
+                if("date" in cal_event["start"]):
+                    new_event["start"]=datetime.strptime(cal_event['start']['date'],"%Y-%m-%d")
+                    new_event["end"]=datetime.strptime(cal_event['end']['date'],"%Y-%m-%d")
+                else:
+                    new_event['start']=datetime.strptime(cal_event["start"]["dateTime"].split("+")[0],"%Y-%m-%dT%H:%M:%S")
+                    new_event['end']= datetime.strptime(cal_event['end']['dateTime'].split("+")[0],"%Y-%m-%dT%H:%M:%S")
+
+                if("hangoutLink" in cal_event):
+                    new_event["hangoutLink"]=cal_event["hangoutLink"]
+                events.append(new_event)                  
+    events.sort(key=lambda x:x["start"], reverse=True)
+    print(events)
